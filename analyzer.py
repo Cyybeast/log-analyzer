939bbd05e51a6a8e28e8b63d8fd82e746530efd6
Cyybeast <nehliirichii123@gmail.com>
2026-04-13 12:51:20 +0100

    Initial commit - Log Analyzer project

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,27 @@
+failed_logins = []
+ip_count = {}
+
 with open("sample.log", "r") as file:
     logs = file.readlines()
 
-print(logs)
+for line in logs:
+    if "Failed login" in line:
+        failed_logins.append(line)
+
+for log in failed_logins:
+    parts = log.split()
+    ip = parts[-1]
+
+    if ip in ip_count:
+        ip_count[ip] += 1
+    else:
+        ip_count[ip] = 1
+
+print("Failed Logins:")
+for log in failed_logins:
+    print(log)
+
+print("\nSuspicious IPs:")
+for ip, count in ip_count.items():
+    if count > 2:
+        print(f"{ip} has {count} failed attempts")
